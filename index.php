c2b573d9a02c1527b65747f862554e8995461ab1
Christine Flynn <43040096+cflynn7@users.noreply.github.com>
2018-09-23 22:36:37 -0400

    took out inline styling for <li>

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         $postTitles = getPostTitlesFromDatabase();
 
         foreach($postTitles as $postTitle) {
-            echo "<li ><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
+            echo "<li><a href='post.php'>" . $postTitle . "</a></li>";
         }
      ?>
      </ul>   
