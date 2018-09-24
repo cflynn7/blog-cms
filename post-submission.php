4abe33421d2808ebadf65203ca40a5130eee00c9
Christine Flynn <43040096+cflynn7@users.noreply.github.com>
2018-09-23 20:05:39 -0400

    Post Submission to PHP

---
+++ post-submission.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+<body>
+<header>
+    
+    <h1>Hello World!</h1>
+
+</header>
+
+<nav>
+        <a href="/index.php">Home</a> |
+        <a href="/post-submission.php">Submit a Post</a> |
+        
+</nav>
+
+<main>
+    <form>
+     Title: <input type="text" name="title"><br>
+     Author: <input type="text" name="authName"><br>
+     Date: <input type="date" name="dateOfPost"><br>
+     Your Blog Entry: <input type="textarea" name="blogContent" id="entryContent"><br>
+     <input type="submit" value="Submit"> 
+    </form>
+</main>
+
+<footer>Christine Flynn / Advanced Web Design / Fall 2018</footer>
+
+
+
+
+<p> </p>
+
+</body>
+</html>
+
