5ba139211cbffc1a4c61c3204967eddce53978cb
Christine Flynn <43040096+cflynn7@users.noreply.github.com>
2018-09-23 20:05:07 -0400

    POST file to PHP

---
+++ post.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+<body>
+<header>
+    
+    <h1>Hello World!</h1>
+
+</header>
+
+<nav>
+        <a href="/index.php">Home</a> |
+        <a href="/post-submission.php">Submit a Post</a> |
+        
+</nav>
+
+<main>
+    <h1>My First Blog Post</h1>
+
+    <div>Author: Christine Flynn</div>
+    <div>September 23, 2018</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a felis eget tortor volutpat tincidunt ut quis nunc. Integer condimentum libero odio, nec vestibulum justo fringilla id. Nulla ullamcorper accumsan nisi at volutpat. Ut imperdiet eros quis dictum sagittis. Suspendisse a ante at sapien ullamcorper accumsan at et diam. Nulla iaculis consectetur lorem, at viverra nibh. Ut rutrum enim non iaculis laoreet. Sed pharetra sodales facilisis. Vivamus pharetra semper imperdiet. Vestibulum quis eros massa.
+
+Duis in porttitor nisl. Maecenas turpis est, gravida ut sem vitae, sollicitudin iaculis magna. Morbi sed cursus odio. Curabitur quis maximus neque. Aliquam sed vestibulum nulla. Donec nisi mauris, dignissim id arcu non, rutrum varius tellus. Mauris vulputate porttitor libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus facilisis lorem, tempus pharetra nunc facilisis non. Integer neque orci, mollis a velit vitae, accumsan feugiat turpis. Nam sed efficitur magna, nec finibus leo. Ut dapibus volutpat magna, sed dignissim nisl venenatis in.
+
+Nunc iaculis auctor ante sed tincidunt. Maecenas eget mi libero. Curabitur sit amet dolor sed nunc maximus commodo. Ut quis eleifend purus. Proin efficitur ultrices facilisis. Mauris eget congue arcu, non tincidunt magna. Suspendisse dolor libero, tempor sit amet maximus non, sagittis a leo. Vivamus ligula ligula, ultrices at ante nec, efficitur rutrum tellus. Praesent et erat faucibus lectus dictum posuere id ac ipsum. Sed purus orci, consequat nec leo id, dapibus fringilla risus. Etiam sodales feugiat luctus. Etiam quis sagittis risus.</div>
+    
+
+
+</main>
+
+<footer>Christine Flynn / Advanced Web Design / Fall 2018</footer>
+
+
+
+
+
+
+</body>
+</html>
