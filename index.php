c2d1bcd771cf680ebfe914176bb47b4bbf59c769
Christine Flynn <43040096+cflynn7@users.noreply.github.com>
2018-09-23 20:03:49 -0400

    index to PHP file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+<body>
+<header>
+    
+    <h1>Hello World!</h1>
+
+</header>
+
+<nav>
+        <a href="/index.html">Home</a> |
+        <a href="/post-submission.html">Submit a Post</a> |
+        
+</nav>
+
+<main>
+    <li><a href="post.html">First Blog Post</a></li>
+</main>
+
+<footer>Christine Flynn / Advanced Web Design / Fall 2018</footer>
+
+
+
+
+<p> </p>
+
+</body>
+</html>
