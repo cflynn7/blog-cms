4a6560d470681e81bf0e87ae4a1e2574e60c1328
Christine Flynn <43040096+cflynn7@users.noreply.github.com>
2018-09-29 21:12:02 -0400

    index

--- index.php
+++ index.php
@@ -1,25 +1,59 @@
+<?php include_once 'db_connect.php'; ?>
+
 <?php include 'header.php'; ?>
 
 <?php include 'nav.php'; ?>
 
+
+
+
+
 <main>
 
+
+<?php
+$conn;
+?>
+
 <?php 
-    function getPostTitlesFromDatabase() {
-        //TODO in Module 4
-        // get this data from a database instead of hardcoding it 
-        $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
-        return $postTitles;
+    
+    function getPostDetailsFromDatabase() {
+     
+        // Get the post title
+
+       $postTitle = rawurldecode($_GET["title"]);
+
+       // Get the post that matches the postTitle
+       include_once 'db_connect.php';
+       $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
+       $result = mysqli_query($conn, $sql);
+
+       // Get the first row from the result in an associative array
+
+        $postDetails = mysqli_fetch_assoc($result);
+        return $postDetails;
     }
-    ?>
+
+?>
+
+    
+   
 
     <ul> 
      <?php 
-        $postTitles = getPostTitlesFromDatabase();
-
-        foreach($postTitles as $postTitle) {
-            echo "<li><a href='post.php'>" . $postTitle . "</a></li>";
-        }
+	function getPostTitlesFromDatabase() {
+	// Get all the post titles from the posts table
+        include_once 'db_connect.php'; 
+	$sql = "SELECT title FROM posts";
+	$result = mysqli_query($conn, $sql); 
+	
+	// Get each result row as an assoc array, then add title to $postTitles
+	$postTitles = array();
+	while($row = mysqli_fetch_assoc($result)){
+         array_push($postTitles, $row['title']);
+	}
+	return $postTitles; 
+}
      ?>
      </ul>   
 
@@ -27,12 +61,8 @@
 
 </main>
 
-
-
-
-
-
 <?php include 'footer.php'; ?>
 
 </body>
 </html>
+
