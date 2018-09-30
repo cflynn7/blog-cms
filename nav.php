ec9368dae1e1bba435962ebe4b5c551dc65da8b1
Christine Flynn <43040096+cflynn7@users.noreply.github.com>
2018-09-29 21:16:02 -0400

    nav post sub 9/29
    nav post sub 9/29

--- nav.php
+++ nav.php
@@ -1,5 +1,10 @@
+
 <nav>
         <a href="/index.php">Home</a> |
         <a href="/post-submission.php">Submit a Post</a> |
         
 </nav>
+
+<h1>BITE MY ASS!</h1>
+
+
--- post-submission.php
+++ post-submission.php
@@ -1,12 +1,16 @@
 <?php include 'header.php'; ?>
+
 <?php include 'nav.php'; ?>
+
+
+
 <main>
-    <form>
+    <form action="submit-post.php" method="POST">
      Title: <input type="text" name="title"><br>
      Author: <input type="text" name="authName"><br>
      Date: <input type="date" name="dateOfPost"><br>
      Your Blog Entry: <input type="textarea" name="blogContent" id="entryContent"><br>
-     <input type="submit" value="Submit"> 
+     <input type="submit" value="Submit" name="submit"> 
     </form>
 </main>
 
